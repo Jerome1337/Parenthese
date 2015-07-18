b983f7702aeeb6a31901f388f172e721d275ff22
Elvis <elvis.perazic@gmail.com>
2015-07-18 19:46:37 +0200

    modif

--- Website/dist/entreprises.php
+++ Website/dist/entreprises.php
@@ -18,20 +18,32 @@
             <section class="row fullWidth titleContainer">
                 <h2><img src="img/entreprises/espace-entreprise.png" alt="Image du titre de la page entreprises"></h2>
             </section>
+<<<<<<< Updated upstream
             <section class="firstContainer">
+=======
+            <section>
+>>>>>>> Stashed changes
                 <section class="row">
                     <h3>Pourquoi nous faire confiance ?</h3>
                     <div class="large-6 medium-6 small-12 columns leftText">
                         <h4>Des artistes de qualités</h4>
                         <p>Nos artistes sont minutieusement selectionné par nos soins. Nous plaçons les artistes en fonction de vos projets. Chaque projet étant unique, nous mettons un point d'honneur à vous offrir ce qu'il y'a de mieux.</p>
                     </div>
+<<<<<<< Updated upstream
                     <div class="large-6 medium-6 small-12 columns pictoWrap">
                         <p class="pictos"></p>
+=======
+                    <div class="large-6 medium-6 small-12 columns">
+                        <div  class="pictos">
+                            <!-- <img src="img/badge.svg" alt="Badge"> -->
+                        </div>
+>>>>>>> Stashed changes
                     </div>
                 </section>
             </section>
             <section class="secondContainer">
                 <section class="row">
+<<<<<<< Updated upstream
                     <div class="large-6 medium-6 small-12 columns pictoWrap">
                         <p class="pictos"></p>
                     </div>
@@ -39,6 +51,17 @@
                         <h4>Des experts à votre écoute</h4>
                         <p>Nos agents sont des experts reconnus dans le monde de l'art. Avec eux, vos projets deviennent réalités.Chaque projet est géré par un expert qui vous accompagne tout au long de votre projet.</p>
                     </div>
+=======
+                    <div class="large-6 medium-6 small-12 columns">
+                        <div  class="pictos2">
+                            <!-- <img src="img/main.svg" alt="Mains"> -->
+                        </div>
+                    </div>
+                    <div class="large-6 medium-6 small-12 columns">
+                        <h3 class="text-left">Des experts à votre écoute</h3>
+                        <p class="text-left">Nos agents sont des experts reconnus dans le monde de l'art. Avec eux, vos projets deviennent réalités.Chaque projet est géré par un expert qui vous accompagne tout au long de votre projet.</p>
+                    </div>
+>>>>>>> Stashed changes
                 </section>
             </section>
             <section class="thirdContainer">
@@ -47,8 +70,15 @@
                         <h4>Des prestations adaptées</h4>
                         <p>Chaque projet étant unique, nous adptons notre offre à votre demande. Nous pouvons réaliser de grand projet comme de petit projet.</p>
                     </div>
+<<<<<<< Updated upstream
                     <div class="large-6 medium-6 small-12 columns pictoWrap">
                         <p class="pictos"></p>
+=======
+                    <div class="large-6 medium-6 small-12 columns">
+                        <div class="pictos">
+                            <!-- <img class="reglage" src="img/reglage.svg" alt="reglage"> -->
+                        </div>
+>>>>>>> Stashed changes
                     </div>
                 </section>
             </section>
